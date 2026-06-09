9caf3e616d2c0180377c8ddd24d9d01f5bd01055
ManiRaj Sai <44311157+thecr7guy2@users.noreply.github.com>
2026-06-09 10:23:09 +0200

    docs: add docstrings and improve formatting in First_gan/train.py

--- First_gan/train.py
+++ First_gan/train.py
@@ -1,10 +1,15 @@
+"""Training script for the baseline GAN on MNIST.
+
+Sets up the generator and discriminator, runs the training loop, and saves
+generated image samples at regular intervals throughout training.
+"""
 import torch
-import torch.nn as nn 
+import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 from dataloader import getdata
-from modules.generator import generator,gen_loss
-from modules.discriminator import discriminator,dis_loss
+from modules.generator import generator, gen_loss
+from modules.discriminator import discriminator, dis_loss
 from tqdm import tqdm
 from torchvision.utils import save_image
 
@@ -19,27 +24,27 @@ hidden_dim = 128
 image_dim = 784
 #####################
 
-def check_gen_images(gen,noise_dim,batch_size,device,epoch):
+
+def check_gen_images(gen, noise_dim, batch_size, device, epoch):
+    """Save a grid of generated images for the current epoch."""
     gen_model.eval()
     dis_model.eval()
     with torch.no_grad():
-        noise_vec = torch.normal(0,1, size=(batch_size,noise_dim))
+        noise_vec = torch.normal(0, 1, size=(batch_size, noise_dim))
         noise_vec = noise_vec.to(device)
         gen_image = gen(noise_vec)
         save_image(gen_image.view(gen_image.size(0), 1, 28, 28), 'gen_images/sample_' + str(epoch) + '.png')
-        
+
     gen_model.train()
     dis_model.train()
-        
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-train_loader,test_loader = getdata()
+train_loader, test_loader = getdata()
 
-
-gen_model = generator(noise_dim,hidden_dim,image_dim).to(device)
-dis_model = discriminator(image_dim,hidden_dim).to(device)
+gen_model = generator(noise_dim, hidden_dim, image_dim).to(device)
+dis_model = discriminator(image_dim, hidden_dim).to(device)
 
 criterion = nn.BCEWithLogitsLoss()
 
@@ -47,69 +52,56 @@ gen_opt = torch.optim.Adam(gen_model.parameters(), lr=learning_rate)
 
 dis_opt = torch.optim.Adam(dis_model.parameters(), lr=learning_rate)
 
-
-gl=[]
-dl=[]
-
+gl = []
+dl = []
 
 for epoch in range(num_epochs):
     running_gen_loss = 0
     running_dis_loss = 0
-    
-    loop=tqdm(train_loader)
-    
+
+    loop = tqdm(train_loader)
+
     gen_model.train()
     dis_model.train()
-    
-    for batch_index,(img,label) in enumerate(loop):
+
+    for batch_index, (img, label) in enumerate(loop):
         batch_size = len(img)
-        
-        img_flatten= img.view(batch_size, -1).to(device)
-        
+
+        img_flatten = img.view(batch_size, -1).to(device)
+
         dis_opt.zero_grad()
-        
-        disc_loss = dis_loss(gen_model,dis_model,criterion,img_flatten,noise_dim,device)
-        
+
+        disc_loss = dis_loss(gen_model, dis_model, criterion, img_flatten, noise_dim, device)
+
         disc_loss.backward(retain_graph=True)
-        
+
         dis_opt.step()
-        
-        
-        
+
         gen_opt.zero_grad()
-        
-        gene_loss=gen_loss(gen_model,dis_model,criterion,img_flatten,noise_dim,device)
-        
+
+        gene_loss = gen_loss(gen_model, dis_model, criterion, img_flatten, noise_dim, device)
+
         gene_loss.backward(retain_graph=True)
-        
+
         gen_opt.step()
-        
-        
-        
+
         running_gen_loss = running_gen_loss + gene_loss.item()
         running_dis_loss = running_dis_loss + disc_loss.item()
-        
+
         if batch_index == 23 and epoch % 2:
-            check_gen_images(gen_model,noise_dim,batch_size,device,epoch)
-        
-    epoch_gen_loss=running_gen_loss/len(train_loader)
-    epoch_dis_loss=running_dis_loss/len(train_loader)
-    
+            check_gen_images(gen_model, noise_dim, batch_size, device, epoch)
+
+    epoch_gen_loss = running_gen_loss / len(train_loader)
+    epoch_dis_loss = running_dis_loss / len(train_loader)
+
     gl.append(epoch_gen_loss)
     dl.append(epoch_dis_loss)
-    
-    print('The loss of the generator is {} and the loss of the discriminator is {} for epoch no. {}'.format(epoch_gen_loss, epoch_dis_loss , epoch))
+
+    print('The loss of the generator is {} and the loss of the discriminator is {} for epoch no. {}'.format(epoch_gen_loss, epoch_dis_loss, epoch))
 
 
 with torch.no_grad():
-        noise_vec = torch.normal(0,1, size=(batch_size*2,noise_dim))
-        noise_vec = noise_vec.to(device)
-        gen_image = gen_model(noise_vec)
-        save_image(gen_image.view(batch_size*2, 1, 28, 28), 'gen_images/final_image.png')        
-        
-
-        
-        
-        
-
-
+    noise_vec = torch.normal(0, 1, size=(batch_size * 2, noise_dim))
+    noise_vec = noise_vec.to(device)
+    gen_image = gen_model(noise_vec)
+    save_image(gen_image.view(batch_size * 2, 1, 28, 28), 'gen_images/final_image.png')
