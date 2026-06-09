4589670e3981f719d289a949ab465d86c734b738
ManiRaj Sai <44311157+thecr7guy2@users.noreply.github.com>
2026-06-09 10:21:50 +0200

    docs: add docstrings and improve formatting in ESRGAN/data_loader.py

--- ESRGAN/data_loader.py
+++ ESRGAN/data_loader.py
@@ -1,3 +1,4 @@
+"""Data loading utilities for ESRGAN super-resolution training and validation."""
 import cv2
 import improc
 from torchvision import datasets, transforms, utils
@@ -8,7 +9,10 @@ import numpy as np
 
 
 class SuperRes(Dataset):
+    """PyTorch Dataset for loading paired HR and LR image pairs for super-resolution."""
+
     def __init__(self, hr_data_dir, hr_image_size, upscale_factor, mode):
+        """Initialize the dataset with directory, image size, scale factor, and split mode."""
         super(SuperRes, self).__init__()
         self.hr_data_dir = hr_data_dir
         self.image_list = os.listdir(hr_data_dir)
@@ -18,9 +22,11 @@ class SuperRes(Dataset):
         self.mode = mode
 
     def __len__(self):
+        """Return the total number of images in the dataset."""
         return len(self.image_list)
 
     def __getitem__(self, index):
+        """Load and return an HR/LR image pair as tensors for the given index."""
         img_name = self.image_list[index]
         image = cv2.imread(os.path.join(self.hr_data_dir, img_name)).astype(np.float32) / 255.
         if self.mode == "Train":
@@ -45,6 +51,7 @@ class SuperRes(Dataset):
 
 
 def get_loader(hr_data_dir, hr_image_size, upscale_factor, mode, batch_size, shuffle):
+    """Build and return a DataLoader for the SuperRes dataset."""
     dataset = SuperRes(hr_data_dir, hr_image_size, upscale_factor, mode)
     d_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=3)
     return d_loader
